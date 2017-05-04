f151be178152046b698fd121c10ca28a9849d9b2
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2017-05-04 15:54:40 -0700

    setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './jsx/app.jsx',
+  output: {
+    path: __dirname + '/js/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loaders: ['babel']
+    }]
+  }
+}
